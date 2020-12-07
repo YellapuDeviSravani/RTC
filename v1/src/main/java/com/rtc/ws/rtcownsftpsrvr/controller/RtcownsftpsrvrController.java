1cc76c933baf793a05d805a416c0b8ba705257b2
Nagrjvv96 <64518349+Nagrjvv96@users.noreply.github.com>
2020-12-07 15:33:10 +0530

    Get SFTP Server Details

--- v1/src/main/java/com/rtc/ws/rtcownsftpsrvr/controller/RtcownsftpsrvrController.java
+++ v1/src/main/java/com/rtc/ws/rtcownsftpsrvr/controller/RtcownsftpsrvrController.java
@@ -1,13 +1,16 @@
 package com.rtc.ws.rtcownsftpsrvr.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rtc.ws.rtcownsftpsrvr.entity.Rtcownsftpsrvr;
 import com.rtc.ws.rtcownsftpsrvr.service.RtcownsftpsrvrService;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 public class RtcownsftpsrvrController {
@@ -15,13 +18,12 @@ public class RtcownsftpsrvrController {
 	@Autowired
 	private RtcownsftpsrvrService rtcownsftpsrvrSer;
 
-	
-	@GetMapping(value = "/getSftpDetails")
-	@ApiOperation(value = "Get SFTP Details")
-	public Iterable<Rtcownsftpsrvr> getSftpDetails() throws Exception {
+	@RequestMapping(value = "/getSftpServerDetails", method = RequestMethod.GET, produces = "application/json")
+	@ApiOperation(value = "Get SFTP server details")
+	@ApiResponses(value = { @ApiResponse(code = 500, message = "Server error"),
+			@ApiResponse(code = 200, message = "List of SFTP server details", response = Rtcownsftpsrvr.class, responseContainer = "List") })
+	public Iterable<Rtcownsftpsrvr> getSftpServerDetails() throws Exception {
 		return rtcownsftpsrvrSer.getRtcownsftpsrvrs();
 	}
-	
+
 }
-
-
--- v1/src/main/java/com/rtc/ws/rtcownsftpsrvr/repository/RtcownsftpsrvrRepository.java
+++ v1/src/main/java/com/rtc/ws/rtcownsftpsrvr/repository/RtcownsftpsrvrRepository.java
@@ -1,9 +1,11 @@
 package com.rtc.ws.rtcownsftpsrvr.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rtc.ws.rtcownsftpsrvr.entity.Rtcownsftpsrvr;
 
+@Repository
 public interface RtcownsftpsrvrRepository extends CrudRepository<Rtcownsftpsrvr, String>{
 	
 	
