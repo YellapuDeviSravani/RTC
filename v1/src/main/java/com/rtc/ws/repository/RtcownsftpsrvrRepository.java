87a276af597e525198dbd53ae575ac88855855c3
Nagrjvv96 <64518349+Nagrjvv96@users.noreply.github.com>
2020-12-07 21:30:33 +0530

    Updated code

--- v1/src/main/java/com/rtc/ws/repository/RtcownsftpsrvrRepository.java
+++ v1/src/main/java/com/rtc/ws/repository/RtcownsftpsrvrRepository.java
@@ -1,12 +1,12 @@
-package com.rtc.ws.rtcownsftpsrvr.repository;
+package com.rtc.ws.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.rtc.ws.rtcownsftpsrvr.entity.Rtcownsftpsrvr;
+import com.rtc.ws.entity.Rtcownsftpsrvr;
 
+// Repository will automatically generate query and get details from the DB
 @Repository
-public interface RtcownsftpsrvrRepository extends CrudRepository<Rtcownsftpsrvr, String>{
-	
-	
+public interface RtcownsftpsrvrRepository extends CrudRepository<Rtcownsftpsrvr, String> {
+
 }
